bce76ed4d68329ee1ca6b0a1ad97fc797483e437
liz <lissete.trejo@gmail.com>
2023-06-29 08:48:30 -0700

    update Card component to handle like/unlike and delete functionalities

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,15 @@
 import { Icon } from '@iconify/react';
 
-const Card = ({id, message, likes_count, date_created}) => {
+const Card = (props) => {
+  const {
+    id, 
+    message, 
+    likes_count, 
+    date_created, 
+    handleLike, 
+    handleDelete
+  } = props;
+
   const formattedDate = (dateString) => {
     const date = new Date(dateString);
     const options = { month: 'short', day: 'numeric', year: 'numeric' };
@@ -8,24 +17,35 @@ const Card = ({id, message, likes_count, date_created}) => {
   };
 
   return (
-    <div className="bg-primary bg-opacity-50 w-72 rounded-lg flex relative">
-      <Icon icon="mdi:pencil" className="text-lg absolute top-0 left-0 m-4"/>
-      <Icon icon="mdi:trash" className="text-lg absolute top-0 right-0 m-4"/>
-      <span className="absolute text-center text-xs top-0 w-full mt-4 font-bold">
-        {formattedDate(date_created)}
-      </span>
-      <span className="font-bold mx-auto text-3xl text-center py-16 px-4">{message}</span>
-      <div className="absolute bottom-0 w-full py-2 flex gap-2 justify-center">
-        <button>
-          <Icon icon="typcn:minus" className="self-center"/>
+    <article className="bg-primary bg-opacity-50 p-4 rounded-lg">
+      <div className="w-72 flex relative h-full">
+        <button className="text-lg absolute top-0 left-0">
+          <Icon icon="mdi:pencil"/>
         </button>
-        <span className="font-bold">{likes_count}</span>
-        <Icon icon="mdi:heart" className="text-lg"/>
-        <button>
-          <Icon icon="typcn:plus" className="self-center"/> 
+        <button 
+          className="text-lg absolute top-0 right-0"
+          onClick={()=>handleDelete(id)}
+        >
+          <Icon icon="mdi:trash" />
         </button>
+        <span className="absolute text-center text-xs top-0 w-full font-bold">
+          {formattedDate(date_created)}
+        </span>
+        <span className="font-bold mx-auto text-3xl text-center py-16 px-4 self-center">
+          {message}
+        </span>
+        <div className="absolute bottom-0 w-full flex gap-2 justify-center">
+          <button onClick={()=>handleLike(id, true)}>
+            <Icon icon="typcn:minus" className="self-center"/>
+          </button>
+          <span className="font-bold">{likes_count}</span>
+          <Icon icon="mdi:heart" className="text-lg text-red-600"/>
+          <button onClick={()=>handleLike(id)}>
+            <Icon icon="typcn:plus" className="self-center"/> 
+          </button>
+        </div>
       </div>
-    </div>
+    </article>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,59 @@
 import Card from './Card';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Home = () => {
-  const cardProps = {
-    "date_created": "Tue, 27 Jun 2023 16:15:12 GMT", 
-    "id": 1, 
-    "likes_count": 11, 
-    "message": "Test Message will go here. "
+  const baseURL = 'http://127.0.0.1:5000/cards'
+  const [cards, setCards] = useState([])
+  useEffect(() => {
+    axios.get(baseURL)
+      .then((response) => {
+        setCards(response.data)
+      }
+    )
+  }, [])
+
+  const handleLike = (id, unlike=false) => {
+    const likeURL = `${baseURL}/${id}/${unlike ? 'unlike' : 'like'}`
+    axios.patch(likeURL)
+      .then((response) => {
+        const new_cards = cards.map((card) => {
+          if (card.id === id) {
+            return response.data.card
+          } else {
+            return card
+          }
+        }
+      )
+      setCards(new_cards)
+    })
+  }
+
+  const handleDelete = (id) => {
+    const deleteURL = `${baseURL}/${id}`
+    axios.delete(deleteURL)
+      .then((response) => {
+        const new_cards = cards.filter((card) => card.id !== id)
+        setCards(new_cards)
+      })
   }
 
   return (
-    <section className="flex flex-col gap-10">
+    <>
       <h1>Login page will go here eventually</h1>
-      <Card {...cardProps}/>
-    </section>
+      <section className="flex flex-wrap gap-4 py-10">
+        {
+          cards.map((card) => (
+            <Card 
+              {...card}
+              key={card.id} 
+              handleLike={handleLike}
+              handleDelete={handleDelete}
+            />
+          ))
+        }
+      </section>
+    </>
   )
 };
 
