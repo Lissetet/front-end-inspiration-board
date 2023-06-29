8b8f6290d9746260643ae61276ba1a1498d09bb2
liz <lissete.trejo@gmail.com>
2023-06-29 11:26:22 -0700

    updates to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import { Icon } from '@iconify/react';
+import { useState, useEffect, useRef } from 'react';
 
 const Card = (props) => {
   const {
@@ -7,7 +8,8 @@ const Card = (props) => {
     likes_count, 
     date_created, 
     handleLike, 
-    handleDelete
+    handleDelete,
+    handleEdit
   } = props;
 
   const formattedDate = (dateString) => {
@@ -16,25 +18,55 @@ const Card = (props) => {
     return date.toLocaleDateString('en-US', options);
   };
 
+  const [textEditable, setTextEditable] = useState(false);
+  const [cardText, setCardText] = useState(message);
+
+  const handleChange = (e) => {
+    setCardText(e.target.value);
+  }
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [textEditable]);
+
+
   return (
     <article className="bg-primary bg-opacity-50 p-4 rounded-lg">
       <div className="w-72 flex relative h-full">
-        <button className="text-lg absolute top-0 left-0">
+        <button 
+          onClick={()=>setTextEditable(!textEditable)}
+          disabled={textEditable}
+          className={`text-lg absolute bottom-0 right-0 z-50 ${textEditable ? 'hidden' : ''}`}
+        >
           <Icon icon="mdi:pencil"/>
         </button>
         <button 
-          className="text-lg absolute top-0 right-0"
+          disabled={textEditable}
+          onClick={()=>handleEdit(id, )}
+          className={`text-lg absolute bottom-0 right-0 z-50 ${textEditable ? '' : 'hidden'}`}
+        >
+          <Icon icon="mdi:check"/>
+        </button>
+        <button 
+          className="text-lg absolute top-0 right-0 z-50"
           onClick={()=>handleDelete(id)}
         >
           <Icon icon="mdi:trash" />
         </button>
-        <span className="absolute text-center text-xs top-0 w-full font-bold">
+        <span className="absolute text-xs top-0 w-full font-bold">
           {formattedDate(date_created)}
         </span>
-        <span className="font-bold mx-auto text-3xl text-center py-16 px-4 self-center">
-          {message}
+        <span 
+          contentEditable={textEditable} 
+          ref={inputRef}
+          onInput={handleChange}
+          className="font-bold mx-auto text-3xl text-center py-16 px-4 self-center"
+        >
+          {cardText}
         </span>
-        <div className="absolute bottom-0 w-full flex gap-2 justify-center">
+        <div className="absolute bottom-0 w-full flex gap-2">
           <button onClick={()=>handleLike(id, true)}>
             <Icon icon="typcn:minus" className="self-center"/>
           </button>
