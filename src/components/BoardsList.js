39979e9bfec02ccf94aec5046a8d2f27bb69c3cd
Luwam Ghebremicael <luwammikael@gmail.com>
2023-06-29 13:55:32 -0700

    adds list of boards on boards list

--- src/components/BoardsList.js
+++ src/components/BoardsList.js
@@ -1,9 +1,26 @@
-const BoardSelection = (prop) => {
+import React from "react"
+
+const BoardsList = ({boards}) => {
+  const displayBoardInfo = () => {
+    return boards.map((board,index)=>{
+      return (
+        <li key={index}>
+          <div>
+          <b>{board.title}</b> by {board.owner}
+          </div>
+          <div>
+          {board.description}
+          </div>
+        </li>
+      )
+    })
+  }
   return (
     <section>
       <h1>Select a Board</h1>
+      <ul>{displayBoardInfo()}</ul>
     </section>
   );
 }
 
-export default BoardSelection;
+export default BoardsList;
