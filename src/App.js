fdbd6744914d379b15823346e269daae28443608
La Tasha Pollard <latashajpollard@gmail.com>
2023-06-29 13:30:35 -0500

    adds Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import { Icon } from '@iconify/react';
-import './App.css';
+import { Icon } from "@iconify/react";
+import "./App.css";
+import BoardList from "./components/BoardList";
 
 function App() {
-  return (
-    <div className="App">
-      <Icon icon="mdi-light:home" />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Icon icon="mdi-light:home" />
+			<header>
+				<h1>Your Boards</h1>
+				<h2> </h2>
+				<BoardList />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import CardList from "./CardList";
+
+const Board = () => {
+	return (
+		<div>
+            <h1> Board Name</h1>
+            <h2> Board Description</h2>
+			<button>Create a Card</button>
+			<CardList />
+		</div>
+	);
+};
+
+export default Board;
