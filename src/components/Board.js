f5f7c728f1f399baec9761cc1e7bcbbfa71a7ca7
lulu <lulusun1210@gmail.com>
2023-06-30 14:58:04 -0500

    render notfoundpage when board not found but still has error shown

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,12 +2,13 @@ import { useParams } from "react-router-dom";
 import CardList from "./CardList";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import NotFound from "./NotFound";
+import { Navigate } from "react-router-dom";
 
 const Board = (prop) => {
 	const { id } = useParams();
 	const baseURL = "http://127.0.0.1:5000/boards";
 	const [board, setBoard] = useState({});
+	const [isError, setIsError] = useState(false)
 
 	useEffect(() => {
 		axios
@@ -15,19 +16,23 @@ const Board = (prop) => {
 			.then((response) => {
 				setBoard(response.data.board);
 				console.log(response.data.board.cards);
+			})
+			.catch((error) => {
+				setIsError(true)
+			})
 
-			});
+			;
 	}, []);
 
 	return (
 		<>
-			<section>
+			{isError && <Navigate to="/error" />}
+			{!isError && <section>
 				<h1> {board.title}</h1>
 				<h2>{board.description}</h2>
-
 				{/* <input type="text"> Create a Card </input> */}
 				<CardList board_id={id} cards={board.cards} />
-			</section>
+			</section>}
 		</>
 	);
 };
