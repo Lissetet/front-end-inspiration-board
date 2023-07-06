a782118b3a0b8bf3b3aa975fd846aef82c432321
Luwam Ghebremicael <luwammikael@gmail.com>
2023-07-05 22:16:05 -0700

    adds hand cursor when hovering over using cursor-pointer, adds a click even handler with the variable clicker() then calls it with onClick={clicker}, adds navigation path with window.location.href=/boards/ + board.id

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -1,11 +1,15 @@
 import React from "react"
 
 const BoardCard = ({ board }) => {
+  const clicker = () => {
+    window.location.href="/boards/" + board.id
+    // alert(board.id)
+  }
   return (
     <article className="bg-primary bg-opacity-50 p-4 rounded-lg">
 
-    <div className="w-72 flex flex-col relative h-52">
-      <div className="font-bold mx-auto text-2xl text-center py-16 px-4 self-center w-full bg-transparent  resize-none">
+    <div className="w-72 flex flex-col relative h-52 text-2xl cursor-pointer" onClick={clicker}>
+      <div className="font-bold mx-auto  text-center py-16 px-4 self-center w-full bg-transparent  resize-none">
         {board.title}
       </div>
       <div className= "text-base">
