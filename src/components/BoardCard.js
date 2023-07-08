b94aef582ecd6b1576831c7571a2935f15ee9f06
Luwam Ghebremicael <luwammikael@gmail.com>
2023-07-07 19:53:18 -0700

    adds <Link> element for clickability

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -1,14 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 const BoardCard = ({ board }) => {
-  const clicker = () => {
-    window.location.href="/boards/" + board.id
-    // alert(board.id)
-  }
+  // const clicker = () => {
+  //   window.location.href="/boards/" + board.id
+  //   // alert(board.id)
+  // }
   return (
-    <article className="bg-primary bg-opacity-50 p-4 rounded-lg">
+    <Link to={`/boards/${board.id}`} className="bg-primary bg-opacity-50 p-4 rounded-lg">
 
-    <div className="w-72 flex flex-col relative h-52 text-2xl cursor-pointer" onClick={clicker}>
+    <div className="w-72 flex flex-col relative h-52 text-2xl cursor-pointer" >
       <div className="font-bold mx-auto  text-center py-16 px-4 self-center w-full bg-transparent  resize-none">
         {board.title}
       </div>
@@ -16,7 +17,7 @@ const BoardCard = ({ board }) => {
         {board.description}
       </div>
     </div>
-    </article>
+    </Link>
 
   )
 }
